c32906e26091182d130b490ee3d2fa0ca8f62047
anufrikanec <3487608@gmail.com>
2022-02-23 22:09:19 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@ const products = [
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (title = 'Товар', price = 99, img = "./img/item_pic.jpeg") => {
     return `<div class="product-item">
                 <h3>${title}</h3>
+                <img class="header__logo" src="${img}" alt="logo">
                 <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
+
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title,item.price));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
